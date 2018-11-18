c31c05352bb31b23a13f25a4a2cd5612734903ee
Arturo Verbel <arturo-verbel@hotmail.com>
2018-11-18 17:27:31 -0500

    Allr eady

---
+++ index.js
@@ -0,0 +1,33 @@
+const awsIot = require('aws-iot-device-sdk');
+const express = require('express')
+const app = express()
+const port = 3000
+
+ 
+app.get('/', (req, res) => res.send('Light led'))
+
+const device = awsIot.device({
+  keyPath: 'certs/8f680e1fc3-private.pem.key',
+  certPath: 'certs/8f680e1fc3-certificate.pem.crt',
+  caPath: 'certs/root-CA.crt',
+  region: 'us-east-1',
+  clientId: 'server',
+  host: 'a1io5eo0eh1c6a-ats.iot.us-east-1.amazonaws.com'
+});
+
+device.on('connect', function() {
+  console.log('Connected');
+});
+
+app.get('/light/:state', function (req, res) {
+    console.log("GET request")
+    const state = req.params.state;
+
+    const data = { light: state};
+    console.log(data);
+    device.publish('LED', JSON.stringify(data));
+
+    res.send(data);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
