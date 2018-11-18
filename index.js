3eab6d828a7d2590931493c7da1d4fa59d9f3eaa
Arturo Verbel <arturo-verbel@hotmail.com>
2018-11-18 17:57:49 -0500

    Index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const awsIot = require('aws-iot-device-sdk');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 
  
 app.get('/', (req, res) => res.send('Light led'))
